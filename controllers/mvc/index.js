cb8dc8bb2f9c7d111fe6b3375804bf9937579953
Batur <heshupai@gmail.com>
2017-02-09 23:37:02 +0800

    home page display items

--- controllers/mvc/index.js
+++ controllers/mvc/index.js
@@ -11,8 +11,25 @@ let rest_index = async ctx => {
 };
 
 let home = async ctx => {
+    // simulate data
+    let data = [
+        {
+            "type": "代取",
+            "location": "第二食堂",
+            "reward": "15元",
+            "date": "2017-08-08"
+        },
+        {
+            "type": "代取",
+            "location": "第二食堂",
+            "reward": "15元",
+            "date": "2017-08-08"
+        }
+    ];
+
     ctx.render('mvc/home.html', {
-        title: '校园资源共享'
+        title: '校园资源共享',
+        data: data
     })
 }
 
