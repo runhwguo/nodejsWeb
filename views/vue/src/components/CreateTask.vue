e0dbaac825bef8d262bbf7dd8dbedd7a5d1f26c1
Batur <heshupai@gmail.com>
2017-03-01 23:46:35 +0800

    add reminder when input is invalid

--- views/vue/src/components/CreateTask.vue
+++ views/vue/src/components/CreateTask.vue
@@ -1,17 +1,20 @@
 <template>
     <div id="main">
-        
-        <mt-field label="姓名" placeholder="Input username"></mt-field>
-        <mt-field label="手机号" placeholder="Input tel"></mt-field>
-        <mt-field label="截止时间" @click.native="open('picker')" placeholder="截止时间" :value="pickerValue | formatDate"></mt-field>
+        <mt-field label="姓名" placeholder="Input username" 
+                :state="nameState" v-model="name"></mt-field>
+        <mt-field label="手机号" placeholder="Input tel" 
+                :state="phoneState" v-model="phone"></mt-field>
+        <mt-field label="截止时间" @click.native="open('picker')" 
+                    placeholder="截止时间" :value="pickerValue | formatDate"></mt-field>
         <mt-datetime-picker
-            ref="picker"
-            type="datetime"
-            v-model="pickerValue">
-        </mt-datetime-picker>
-        <mt-field label="详请" placeholder="Input detail" type="textarea"></mt-field>
-        <mt-field label="报酬" placeholder="Input reward"></mt-field>
-        <mt-button @click.native="openToast" type="primary" style="margin-top:20px">发布</mt-button>
+            ref="picker" type="datetime"
+            v-model="pickerValue"></mt-datetime-picker>
+        <mt-field label="详请" placeholder="Input detail" type="textarea" 
+                    :state="detailState" v-model="detail"></mt-field>
+        <mt-field label="报酬" placeholder="Input reward" 
+                    :state="rewardState" v-model="reward"></mt-field>
+        <mt-button @click.native="openToast" type="primary"
+                    style="margin-top:20px">发布</mt-button>
     </div>
 </template>
 
@@ -28,7 +31,18 @@
     export default{
         name: "createTask",
         data () {
-            return { pickerValue: new Date()}
+            return {
+                name: "",
+                phone: "",
+                detail: "",
+                reward: "",
+                pickerValue: new Date(),
+                nameState: "",
+                phoneState: "",
+                detailState: "",
+                textarea: "",
+                rewardState: ""
+            }
         },
         methods: {
             open(picker) {
@@ -40,6 +54,20 @@
             },
             openToast() {
                 Toast("正在发布..")
+            }
+        },
+        watch: {
+            name: function(val) {
+                Boolean(val)?this.nameState="success":this.nameState="warning"
+            },
+            phone: function(val) {
+                Boolean(val)?this.phoneState="success":this.phoneState="warning"
+            },
+            detail: function(val) {
+                Boolean(val)?this.detailState="success":this.detailState="warning"
+            },
+            reward: function(val) {
+                Boolean(val)?this.rewardState="success":this.rewardState="warning"
             }
         },
         filters: {
