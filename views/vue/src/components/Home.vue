04a7426ee89c986dc7103a53c9799c7eadc9c2ae
Batur <heshupai@gmail.com>
2017-03-02 20:09:14 +0800

    home page, add swipe

--- views/vue/src/components/Home.vue
+++ views/vue/src/components/Home.vue
@@ -1,9 +1,19 @@
 <template>
-    <h1>home</h1>
+  <swipe class="my-swipe">
+  <swipe-item class="slide1"></swipe-item>
+  <swipe-item class="slide2"></swipe-item>
+  <swipe-item class="slide3"></swipe-item>
+</swipe>
 </template>
 
 
 <script>
+
+import Vue from 'vue';
+import { Swipe, SwipeItem } from 'vue-swipe';
+Vue.component('swipe', Swipe);
+Vue.component('swipe-item', SwipeItem);
+
 export default {
     name: "home",
     data() {
@@ -11,3 +21,26 @@ export default {
     }
 }
 </script>
+<style>
+.my-swipe {
+  height: 200px;
+  color: #fff;
+  font-size: 30px;
+  text-align: center;
+}
+
+.slide1 {
+  background-color: #0089dc;
+  color: #fff;
+}
+
+.slide2 {
+  background-color: #ffd705;
+  color: #000;
+}
+
+.slide3 {
+  background-color: #ff2d4b;
+  color: #fff;
+}
+</style>
