a698af0197cda510c91b3bf0d10534928d2d00d5
Batur <heshupai@gmail.com>
2017-03-03 00:08:31 +0800

    完善主页内容，Me组件细节调整

--- views/vue/src/components/Home.vue
+++ views/vue/src/components/Home.vue
@@ -1,9 +1,20 @@
 <template>
+<div>
   <swipe class="my-swipe">
-  <swipe-item class="slide1"><img src="../assets/logo.png" alt=""></swipe-item>
-  <swipe-item class="slide2"><img src="../assets/logo.png" alt=""></swipe-item>
-  <swipe-item class="slide3"><img src="../assets/logo.png" alt=""></swipe-item>
-</swipe>
+    <swipe-item class="slide1"><img src="../assets/logo.png" alt=""></swipe-item>
+    <swipe-item class="slide2"><img src="../assets/logo.png" alt=""></swipe-item>
+    <swipe-item class="slide3"><img src="../assets/logo.png" alt=""></swipe-item>
+  </swipe>
+
+  <mt-search placeholder="搜索"></mt-search>
+
+  <div class="topDistance" v-for="item in 4">
+    <mt-cell title="[代取]第二食堂" label="[悬赏]15元" >
+      <mt-button type="primary" size="small">接单</mt-button>
+      <mt-button type="default" size="small">置顶</mt-button>
+    </mt-cell>
+  </div>
+</div>
 </template>
 
 
@@ -11,8 +22,11 @@
 
 import Vue from 'vue';
 import { Swipe, SwipeItem } from 'vue-swipe';
+import { Button, Search } from 'mint-ui'
+
 Vue.component('swipe', Swipe);
 Vue.component('swipe-item', SwipeItem);
+Vue.component(Search.name, Search);
 
 export default {
     name: "home",
@@ -22,6 +36,14 @@ export default {
 }
 </script>
 <style>
+
+.topDistance{
+  margin-top: 20px
+}
+.mint-search{
+  height: 100%
+}
+
 .my-swipe {
   height: 200px;
   color: #fff;
--- views/vue/src/components/Me.vue
+++ views/vue/src/components/Me.vue
@@ -1,14 +1,14 @@
 <template>
     <div>
         <img src="../assets/logo.png" alt="profile">
-        <mt-cell title="未完成任务" to="/unfinishedTask">
+        <mt-cell title="未完成任务" to="/unfinishedTask" is-link>
             <mt-badge type="error" size="small">10</mt-badge>
         </mt-cell>
-        <mt-cell title="已完成任务" to="/finishedTask">
+        <mt-cell title="已完成任务" to="/finishedTask" is-link>
             <mt-badge type="success" size="small">3</mt-badge>
         </mt-cell>
-        <mt-cell title="发布的任务"></mt-cell>
-        <mt-cell title="我的信息"></mt-cell>
+        <mt-cell title="发布的任务" is-link></mt-cell>
+        <mt-cell title="我的信息" is-link></mt-cell>
     </div>
 </template>
 
