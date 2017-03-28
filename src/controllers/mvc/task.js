98db4eb8ad33856815dffdc2ff816da9e65107d1
runhwguo <444347292@qq.com>
2017-03-29 01:49:58 +0800

    my task show is begining

--- src/controllers/mvc/task.js
+++ src/controllers/mvc/task.js
@@ -2,47 +2,29 @@ import {Task, User} from "../../tools/model";
 import * as Dao from "../../tools/dao";
 import {TASK_STATE, TASK_TYPE} from "../../models/Task";
 
-const unfinishedTasks = async ctx => {
+const mineTask = async ctx => {
+  let type = ctx.params.type;
+  let attributes = ['id', 'type'];
+  if (type === 'completed') {
+    attributes.push('detail', 'reward');
+  } else if (type === 'unfinished') {
+    attributes.push('detail', 'deadline');
+  } else if (type === 'published') {
+    attributes.push('defail', 'state');
+  }
   let data = await Dao.findAll(Task, {
-    attributes: ['id', 'type', 'deadline', 'detail', 'filename', 'reward'],
-    where: {
-      state: TASK_STATE.COMPLETING
-    }
-  });
+      attributes: attributes,
+      where: {
+        state: TASK_STATE.COMPLETING
+      }
+    });
 
   ctx.render(`my_task_list`, {
     title: '未完成任务',
     data: data
   })
 };
-const completedTasks = async ctx => {
-  let data = await Dao.findAll(Task, {
-    attributes: ['id', 'type', 'deadline', 'detail', 'filename', 'reward'],
-    where: {
-      state: TASK_STATE.COMPLETED,
-      receiveTaskUserId: ctx.state.user.id
-    }
-  });
 
-  ctx.render(`my_task_list`, {
-    title: '已完成的任务',
-    data: data
-  })
-};
-const publishedTasks = async ctx => {
-  let data = await Dao.findAll(Task, {
-    attributes: ['id', 'type', 'deadline', 'detail', 'filename', 'reward'],
-    where: {
-      publishUserId: ctx.state.user.id
-    },
-    limit: 5
-  });
-
-  ctx.render(`my_task_list`, {
-    title: '发布的任务',
-    data: data
-  })
-};
 let myInfo = async ctx => {
   let user = await User.findById(ctx.state.user.id);
 
@@ -72,7 +54,7 @@ const detail = async ctx => {
   })
 };
 
-const taskList = async ctx => {
+const takeList = async ctx => {
   let where = ctx.query.where;
   ctx.render(`take_task_list`, {
     title: TASK_TYPE[where],
@@ -81,10 +63,8 @@ const taskList = async ctx => {
 };
 
 module.exports = {
-  'GET /unfinishedTasks': unfinishedTasks,
-  'GET /completedTasks': completedTasks,
-  'GET /publishedTasks': publishedTasks,
-  'GET /taskList': taskList,
+  'GET /task/mine/:type': mineTask,
+  'GET /task/taskList': takeList,
   'GET /myInfo': myInfo,
   'GET /task/detail/:id': detail
 };
