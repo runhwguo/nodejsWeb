4105fdf5bdf5e976eb4111889f4042784e020f38
RunhwGuo <444347292@qq.com>
2017-04-06 21:48:52 +0800

    修改程锡慧的提议

--- src/tools/cookie.js
+++ src/tools/cookie.js
@@ -1,18 +1,24 @@
 import sha1 from 'sha1';
 import {User} from './model';
-import {session} from './config';
+import {session, admin} from './config';
 
-const cookie2user = async cookie => {
+const cookie2user = async (cookie,cookieName) => {
   if (cookie) {
     let cookieElements = cookie.split('-');
     if (cookieElements.length === 3) {
       // auth maxAge
       let [id, expires, sha1Str] = cookieElements;
       if (expires > Math.round(Date.now() / 1000)) {
-        let user = await User.findByPrimary(id);
-        if (user) {
-          if (sha1Str === sha1(`${user.id}-${user.password}-${expires}-${session.cookieName}`)) {
-            return user;
+        if(cookieName===session.userCookieName){
+          let user = await User.findByPrimary(id);
+          if (user) {
+            if (sha1Str === sha1(`${user.id}-${user.password}-${expires}-${session.userCookieName}`)) {
+              return user.dataValues;
+            }
+          }
+        } else if(cookieName===session.adminCookieName){
+          if (sha1Str === sha1(`${admin.username}-${admin.password}-${expires}-${session.adminCookieName}`)) {
+            return admin;
           }
         }
       }
@@ -21,11 +27,11 @@ const cookie2user = async cookie => {
 };
 
 //build cookieName string by: id-expires-sha1
-const user2cookie = (id, password) => {
+const user2cookie = (id, password,cookieName)=>{
   let expires = Math.round(Date.now() / 1000 + session.maxAge);
-  return `${id}-${expires}-` + sha1(`${id}-${password}-${expires}-${session.cookieName}`);
+  return `${id}-${expires}-` + sha1(`${id}-${password}-${expires}-${cookieName}`);
 };
 
 export {
-  cookie2user, user2cookie
+  cookie2user,user2cookie
 };
