349db3190ab288289eb876141b225c4687f2d366
RunhwGuo <444347292@qq.com>
2017-04-20 22:11:18 +0800

    test wx pay

--- src/controllers/rest/wechat.js
+++ src/controllers/rest/wechat.js
@@ -1,14 +1,14 @@
 import crypto from  'crypto';
 
-const TOKEN = "FuckQ";
+const TOKEN = 'FuckQ';
 // 测试号
-// const APP_ID = "wx4e4e65e636691304";
-// const APP_SECRET = "267497e487074a081b2bf0d609169bb7";
-// const ENCODING_AES_KEY = "MdnDaEKiUmUOPK8YGYuvimwMbpp0rd8lZZ7lwai7vmN";
+// const APP_ID = 'wx4e4e65e636691304';
+// const APP_SECRET = '267497e487074a081b2bf0d609169bb7';
+// const ENCODING_AES_KEY = 'MdnDaEKiUmUOPK8YGYuvimwMbpp0rd8lZZ7lwai7vmN';
 // 平台
-const APP_ID = "wx90eb6b04dcbf5fb2";
-const APP_SECRET = "7e87a72a56080c466d9256387016c886";
-const ENCODING_AES_KEY = "zhzyeMf9jzO9HEiVmCzi3KDGPjyxUyJUFyh1AkI7SfE";
+const APP_ID = 'wx90eb6b04dcbf5fb2';
+const APP_SECRET = '7e87a72a56080c466d9256387016c886';
+const ENCODING_AES_KEY = 'zhzyeMf9jzO9HEiVmCzi3KDGPjyxUyJUFyh1AkI7SfE';
 const GET_ACCESS_TOKEN = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=APPSECRET';//get
 const GET_WX_IP = 'https://api.weixin.qq.com/cgi-bin/getcallbackip?access_token=ACCESS_TOKEN';
 
@@ -27,7 +27,7 @@ const check = async ctx => {
 };
 
 const checkIsFromWxServer = async(signature, timestamp, nonce) => {
-  let code = crypto.createHash("sha1").update([TOKEN, timestamp, nonce].sort().toString().replace(/,/g, ""), 'utf-8').digest("hex");
+  let code = crypto.createHash('sha1').update([TOKEN, timestamp, nonce].sort().toString().replace(/,/g, ''), 'utf-8').digest('hex');
 
   return code === signature;
 };
--- static/js/wx_pay.js
+++ static/js/wx_pay.js
@@ -2,3 +2,32 @@ function isSupportWxPay() {
   let wxInfo = window.navigator.userAgent.match(/MicroMessenger\/([\d.]+)/i);
   return wxInfo && wxInfo[1] >= '5.0';
 }
+
+function onBridgeReady() {
+  WeixinJSBridge.invoke(
+    'getBrandWCPayRequest', {
+      appId: 'wx90eb6b04dcbf5fb2',     //公众号名称，由商户传入
+      timeStamp: `${Number.parseInt(Date.now() / 1000, 10)}`,         //时间戳，自1970年以来的秒数
+      nonceStr: 'e61463f8efa94090b1f366cccfbbb444', //随机串
+      package: 'prepay_id=u802345jgfjsdfgsdg888',
+      signType: 'MD5',         //微信签名方式：
+      paySign: '70EA570631E4BB79628FBCA90534C63FF7FADD89' //微信签名
+    },
+    res => {
+      alert(res);
+      if (res.err_msg === 'get_brand_wcpay_request:ok') {
+        alert('pay success');
+      }     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
+    }
+  );
+}
+if (typeof WeixinJSBridge === 'undefined') {
+  if (document.addEventListener) {
+    document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
+  } else if (document.attachEvent) {
+    document.attachEvent('WeixinJSBridgeReady', onBridgeReady);
+    document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
+  }
+} else {
+  onBridgeReady();
+}
