73a7290d9ee309153285e6be910aaefa5d8202c3
RunhwGuo <444347292@qq.com>
2017-04-20 22:00:46 +0800

    test

--- static/js/wx_pay.js
+++ static/js/wx_pay.js
@@ -1,6 +1,6 @@
 function isSupportWxPay() {
   let wxInfo = window.navigator.userAgent.match(/MicroMessenger\/([\d.]+)/i);
-  let result = !wxInfo && wxInfo[1] >= '5.0';
+  let result = wxInfo && wxInfo[1] >= '5.0';
   alert(window.navigator.userAgent);
   alert(wxInfo);
   alert(wxInfo[1]);
