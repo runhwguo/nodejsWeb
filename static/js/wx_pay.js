b8b48195eec17baae944a839d6165c9083ce1e17
RunhwGuo <444347292@qq.com>
2017-04-20 22:02:11 +0800

    判断微信浏览器>=5.0

--- static/js/wx_pay.js
+++ static/js/wx_pay.js
@@ -1,9 +1,4 @@
 function isSupportWxPay() {
   let wxInfo = window.navigator.userAgent.match(/MicroMessenger\/([\d.]+)/i);
-  let result = wxInfo && wxInfo[1] >= '5.0';
-  alert(window.navigator.userAgent);
-  alert(wxInfo);
-  alert(wxInfo[1]);
-  alert(result);
-  return result;
+  return wxInfo && wxInfo[1] >= '5.0';
 }
