36922f5c4e973b1459db4ff25a3c1b8724abe52c
RunhwGuo <444347292@qq.com>
2017-04-21 16:08:00 +0800

    wechat pay完善

--- src/controllers/rest/wechat.js
+++ src/controllers/rest/wechat.js
@@ -1,4 +1,4 @@
-import crypto from  'crypto';
+import crypto from "crypto";
 
 const TOKEN = 'FuckQ';
 // 测试号
@@ -8,6 +8,11 @@ const TOKEN = 'FuckQ';
 // 平台
 const APP_ID = 'wx90eb6b04dcbf5fb2';
 const APP_SECRET = '7e87a72a56080c466d9256387016c886';
+const MCH_ID = '1462750902';
+const DEVICE_INFO = 'WEB';
+const nonce_str = Math.random().toString();
+
+
 const ENCODING_AES_KEY = 'zhzyeMf9jzO9HEiVmCzi3KDGPjyxUyJUFyh1AkI7SfE';
 const GET_ACCESS_TOKEN = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=APPSECRET';//get
 const GET_WX_IP = 'https://api.weixin.qq.com/cgi-bin/getcallbackip?access_token=ACCESS_TOKEN';
@@ -26,12 +31,18 @@ const check = async ctx => {
   }
 };
 
-const checkIsFromWxServer = async(signature, timestamp, nonce) => {
+const checkIsFromWxServer = async (signature, timestamp, nonce) => {
   let code = crypto.createHash('sha1').update([TOKEN, timestamp, nonce].sort().toString().replace(/,/g, ''), 'utf-8').digest('hex');
 
   return code === signature;
 };
 
+
+const unifiedOrder = async () => {
+
+};
+
 module.exports = {
-  'GET /api/wechat/': check
+  'GET /api/wechat/': check,
+  'GET /api/wechat/pay/unifiedOrder': unifiedOrder
 };
---
+++ src/tools/wx_pay.js
@@ -0,0 +1,39 @@
+import crypto from "crypto";
+
+const paySign = (appid, attach, body, mch_id, nonce_str, notify_url, openid, out_trade_no, spbill_create_ip, total_fee, trade_type) => {
+  let ret = {
+    appid: appid,
+    attach: attach,
+    body: body,
+    mch_id: mch_id,
+    nonce_str: nonce_str,
+    notify_url: notify_url,
+    openid: openid,
+    out_trade_no: out_trade_no,
+    spbill_create_ip: spbill_create_ip,
+    total_fee: total_fee,
+    trade_type: trade_type
+  };
+  let string = _raw(ret);
+  let key = '';
+  string += `&key=${key}`;  //key为在微信商户平台(pay.weixin.qq.com)-->账户设置-->API安全-->密钥设置
+  return crypto.createHash('md5').update(string, 'utf8').digest('hex');
+};
+
+const _raw = args => {
+  let keys = Object.keys(args);
+  keys = keys.sort();
+  let newArgs = {};
+  keys.forEach(key => newArgs[key.toLowerCase()] = args[key]);
+
+  let string = '';
+  for (let k of newArgs) {
+    string += `&${k}'=${newArgs[k]}`;
+  }
+  string = string.substr(1);
+  return string;
+};
+
+export {
+  paySign
+};
