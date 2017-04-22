034f4ef20ab40752ab2cef098ef5a64373ac2e21
runhwguo <444347292@qq.com>
2017-04-23 00:12:08 +0800

    test get open id code

--- src/app.js
+++ src/app.js
@@ -6,7 +6,6 @@ import templating from "./tools/templating";
 import {cookie2user} from "./tools/cookie";
 import staticFiles from "./tools/static_files";
 import {restify} from "./tools/rest";
-import {getCode} from "./tools/wx_pay";
 import {project, session} from "./tools/config";
 import appRootDir from "app-root-dir";
 
@@ -20,12 +19,6 @@ app.use(logger());
 
 // auth
 app.use(async (ctx, next) => {
-  ////// for test start /////
-  await getCode();
-
-  ////// for test end /////
-
-
   let userLoginCookie = ctx.cookies.get(session.userCookieName);
   let user = await cookie2user(userLoginCookie, session.userCookieName);
   if (user) {
@@ -49,17 +42,7 @@ app.use(async (ctx, next) => {
   }
 });
 
-//在生产环境下，
-// 静态文件是由部署在最前面的反向代理服务器（如Nginx）处理的，
-// Node程序不需要处理静态文件。
-// 而在开发环境下，我们希望koa能顺带处理静态文件，
-// 否则，就必须手动配置一个反向代理服务器，
-// 这样会导致开发环境非常复杂
-// TODO
-// if (!isProduction) {
-// middleware
 app.use(staticFiles('/static/', `${appRootDir.get()}/static`));
-// }
 // 解析原始request请求，nodejs的request和koa的request都不解析request
 app.use(bodyParser());
 // 给ctx加上render()来使用Nunjucks middleware
