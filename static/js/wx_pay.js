8ddd6c7035bcda4725f31397988213539b20cb2e
runhwguo <444347292@qq.com>
2017-04-23 21:51:51 +0800

    wechat pay test

--- static/js/wx_pay.js
+++ static/js/wx_pay.js
@@ -4,18 +4,20 @@ function _isSupportWxPay() {
 }
 
 function onBridgeReady(fee) {
-  $.get('/api/wechat/pay/start?fee=' + fee, result => {
-    alert(result);
-    if (result) {
-      WeixinJSBridge.invoke('getBrandWCPayRequest', result, res => {
-          alert(res);
-          if (res.err_msg === 'get_brand_wcpay_request:ok') {
-            alert('pay success');
-          }     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
-        }
-      );
+  $.get('/api/wechat/pay/start', {fee: fee}, result => {
+      alert(result);
+      if (result) {
+        WeixinJSBridge.invoke('getBrandWCPayRequest', result, res => {
+            alert(res);
+            if (res.err_msg === 'get_brand_wcpay_request:ok') {
+              alert('pay success');
+            }     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
+          }
+        );
+      }
     }
-  });
+  )
+  ;
 }
 
 function startPay(fee) {
