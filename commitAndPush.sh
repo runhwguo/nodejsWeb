bb39740e9791530e68853fe37b43240943abf1b9
runhwguo <444347292@qq.com>
2017-04-25 00:49:20 +0800

    悬赏 付钱逻辑 test

--- commitAndPush.sh
+++ commitAndPush.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 git add *
-git commit -m "悬赏 付钱逻辑"
+git commit -m "悬赏 付钱逻辑 test"
 git push
---
+++ static/js/create_task.js
@@ -0,0 +1,158 @@
+$(() => {
+  let form = $('form'),
+    submitBtnWord = $('.ladda-label'),
+    deadline = $('#deadline'),
+    createTask = $('#createTaskTab');
+  deadline.datepicker({
+    minDate: 0,
+    maxDate: 31,
+    autoclose: true,
+    dateFormat: 'yy-mm-dd',
+    onSelect: () => {
+      form.data('bootstrapValidator')
+        .updateStatus('deadline', 'NOT_VALIDATED', null)
+        .validateField('deadline');
+    }
+  });
+
+  deadline.click((event) => {
+    let uiDatepickerDiv = $('#ui-datepicker-div');
+    let viewportWidth = $(window).width();
+    let viewportHeight = $(window).height();
+    let datepickerWidth = uiDatepickerDiv.width();
+    let datepickerHeight = uiDatepickerDiv.height();
+    let leftPos = (viewportWidth - datepickerWidth) / 2; //Standard centering method
+    let topPos = (viewportHeight - datepickerHeight) / 2; //Standard centering method
+    uiDatepickerDiv.css({
+      left: leftPos,
+      top: topPos,
+      position: 'absolute'
+    });
+  });
+
+  form.bootstrapValidator({
+    message: 'The form is not valid',
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',
+      invalid: 'glyphicon glyphicon-remove',
+      validating: 'glyphicon glyphicon-refresh'
+    },
+    fields: {
+      type: {
+        message: 'The type is not valid',
+        validators: {
+          notEmpty: {
+            message: '请选择任务类型'
+          }
+        }
+      },
+      shareCount: {
+        message: 'The type is not valid',
+        validators: {
+          notEmpty: {
+            message: '请填写共享次数'
+          }
+        }
+      },
+      title: {
+        message: 'The title is not valid',
+        validators: {
+          notEmpty: {
+            message: '请填入任务标题'
+          }
+        }
+      },
+      reward: {
+        message: 'The reward is not valid',
+        validators: {
+          regexp: {
+            regexp: /^[0-9]\d*$/,
+            message: "请输入正整数的钱数"
+          }
+        }
+      },
+      deadline: {
+        message: 'The deadline is not valid',
+        validators: {
+          notEmpty: {
+            message: '请选择结束日期'
+          }
+        }
+      }
+    }
+  }).on('success.form.bv', e => {
+    e.preventDefault();
+
+    const doSubmit = () => {
+      let submit = $('.submit'),
+        loading = Ladda.create(submit.get(0));
+      loading.start();
+      let serializeArray = form.serializeArray(),
+        formData = {};
+
+      $.map(serializeArray, (n, i) => formData[n['name']] = n['value'].replace(/"/g, '\\"'));
+      $.ajaxFileUpload({
+        type: form.attr('method'),
+        url: form.attr('action'),
+        secureuri: false,
+        fileElementId: 'file',
+        data: JSON.parse(JSON.stringify(formData)),
+        dataType: 'json',
+        success: (data, status) => {
+          console.log(data, status);
+          submitBtnWord.text('成功');
+          setTimeout(() => {
+            submitBtnWord.text('发布');
+            loading.stop();
+            // bootstrapValidator本身有prevent double click的逻辑，但是和Ladda有互相影响，手动加一下
+            submit.attr('disabled', true);
+          }, 500);
+        },
+        error: (xhr, status, e) => {
+          submitBtnWord.text('失败');
+          setTimeout(() => {
+            submitBtnWord.text('发布');
+            loading.stop();
+          }, 500);
+        }
+      });
+    };
+
+    let rewardType = $('#rewardType')[0].value;
+    alert(rewardType);
+    if (rewardType === '悬赏') {
+      let reward = $('#reward')[0].value;
+      alert(reward);
+      startPay(reward * 100, () => {
+        doSubmit();
+      }, () => {
+      });
+    } else {
+      doSubmit();
+    }
+  });
+  createTask.attr('href', 'javascript:void(0)');
+  let selectpicker = $('.selectpicker');
+  selectpicker.change(() => {
+      if (selectpicker[0].value === '会员共享') {
+        $('#shareCountDiv').show(300);
+      } else {
+        $('#shareCountDiv').hide(300);
+      }
+    }
+  );
+  $('li[name="rewardValue"]').click(function () {
+    console.log(this);
+    $('#rewardType').text($(this).text());
+  });
+
+  $.uploadPreview({
+    input_field: "#file",   // Default: .file
+    preview_box: "#image-preview",  // Default: .image-preview
+    label_field: "#image-label",    // Default: .image-label
+    label_default: "图片补充",   // Default: Choose File
+    label_selected: "更换图片",  // Default: Change File
+    no_label: false                 // Default: false
+  });
+  form.css('margin-bottom', 15 + $('.navbar-fixed-bottom').height());
+});
