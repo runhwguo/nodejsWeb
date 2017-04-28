eb5f971af94f3265c4606534d65abb8f31ca37a6
runhwguo <444347292@qq.com>
2017-04-29 01:31:43 +0800

    wechat pay use third party, 'yeah things always has two aspects', i create, i can learn, but waste time, use third party, is effective

--- commitAndPush.sh
+++ commitAndPush.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 git add *
-git commit -m "wechat pay refund cert"
+git commit -m "wechat pay use third party, 'yeah things always has two aspects', i create, i can learn, but waste time, use third party, is effective"
 git push
--- src/tools/old_wx_pay.js
+++ src/tools/old_wx_pay.js
@@ -20,10 +20,11 @@ const APP_SECRET = '7e87a72a56080c466d9256387016c886';
 const MCH_ID = '1462750902';
 const TRADE_TYPE = 'JSAPI';
 const MCH_KEY = 'guohaoweilovechengxihuiforeveruu';//key为在微信商户平台(pay.weixin.qq.com)-->账户设置-->API安全-->密钥设置
-const NOTIFY_URL = 'http://i-sharing.xyz/api/wechat/order/notify';
-
 
 const MCH_PAU_URL = 'https://api.mch.weixin.qq.com/';
+
+const URL_UNIFIED_ORDER = `${ MCH_PAU_URL }pay/unifiedorder`;
+const URL_REFUND = `${ MCH_PAU_URL }secapi/pay/refund`;
 
 const URL_WX_OPEN_ID_ACCESS_TOKEN = `https://api.weixin.qq.com/sns/oauth2/access_token?appid=${ APP_ID }&secret=${ APP_SECRET }&code=CODE&grant_type=authorization_code`;
 
@@ -85,32 +86,56 @@ const unifiedOrder = async ctx => {
 
   WXPay.createUnifiedOrder({
     body: body,
-    out_trade_no: '20170428' + Math.random().toString().substr(2, 10),
+    out_trade_no: '20170428'+Math.random().toString().substr(2, 10),
     total_fee: 1,
     spbill_create_ip: spbillCreateIp,
-    notify_url: NOTIFY_URL,
+    notify_url: 'http://i-sharing.xyz/api/wechat/order/notify',
     trade_type: TRADE_TYPE
-  }, function (err, result) {
+  }, function(err, result){
     console.log(result);
   });
+
+
+  let notifyUrl = 'http://i-sharing.xyz/api/wechat/order/notify';
+  let totalFee = Number.parseInt(ctx.query.fee);
+  let nonceStr = Math.random().toString();
+  let openid = ctx.cookies.get(config.session.wxOpenId);
+  let outTradeNo = ctx.query.outTradeNo || Date.now() + '';
+
+  let data = {
+    appid: APP_ID,// appid
+    body: body,// 商品或支付单简要描述
+    mch_id: MCH_ID,// 商户号
+    nonce_str: nonceStr,// 随机字符串，不长于32位
+    notify_url: notifyUrl,// 支付成功后微信服务器通过POST请求通知这个地址
+    openid: openid,// 为微信用户在商户对应appid下的唯一标识
+    out_trade_no: outTradeNo,//订单号
+    spbill_create_ip: spbillCreateIp,//终端IP
+    total_fee: totalFee,//金额
+    trade_type: TRADE_TYPE,//NATIVE会返回code_url ，JSAPI不会返回
+  };
+
+  return await _payApiRequest(data, URL_UNIFIED_ORDER);
 };
 
 const refund = async param => {
   logger.log('refund');
+  let nonceStr = Math.random().toString();
+  let outTradeNo = param.outTradeNo;
   let totalFee = Math.abs(param.reward);
-
-  let params = {
-    appid: APP_ID,
-    mch_id: MCH_ID,
+  let data = {
+    appid: APP_ID,// appid
+    mch_id: MCH_ID,// 商户号
+    nonce_str: nonceStr,// 随机字符串，不长于32位
     op_user_id: MCH_ID,
-    out_refund_no: '20170428' + Math.random().toString().substr(2, 10),
-    total_fee: totalFee, //原支付金额
-    refund_fee: totalFee, //退款金额
-    transaction_id: '微信订单号'
+    out_refund_no: outTradeNo,
+    out_trade_no: outTradeNo,//订单号
+    refund_fee: totalFee,
+    total_fee: totalFee,//金额
+    transaction_id: ''
   };
-  WXPay.refund(params, function (err, result) {
-    console.log('refund', arguments);
-  });
+
+  return await _payApiRequest(data, URL_REFUND);
 };
 
 const getAccessTokenOpenId = async code => {
@@ -122,18 +147,28 @@ const getAccessTokenOpenId = async code => {
   return resObj.openid;
 };
 
-const getOnBridgeReadyRequest = async ctx => {
-  wxpay.getBrandWCPayRequestParams({
-    openid: ctx.cookies.get(config.session.wxOpenId),
-    body: ctx.query.body,
-    detail: ctx.query.detail,
-    out_trade_no: '20170428' + Math.random().toString().substr(2, 10),
-    total_fee: ctx.query.totalFee,
-    spbill_create_ip: ctx.ip,
-    notify_url: NOTIFY_URL
-  }, function (err, result) {
-    console.log('refund', arguments);
+const getOnBridgeReadyRequest = async prepay_id => {
+
+  let data = {
+    appId: APP_ID,     //公众号名称，由商户传入
+    timeStamp: '' + Math.round(Date.now() / 1000),         //时间戳，自1970年以来的秒数
+    nonceStr: Math.random().toString(), //随机串
+    package: `prepay_id=${ prepay_id }`,
+    signType: 'MD5' //微信签名方式：
+  };
+
+  let paySign = _paySign(data);
+
+  logger.log(paySign);
+
+
+  let request = Object.assign(data, {
+    paySign: paySign //微信签名
   });
+
+  logger.log(request);
+
+  return request;
 };
 
 export {
