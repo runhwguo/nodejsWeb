4cfb74d2e9bdc8967fb838a5d3655378f40f4b1d
runhwguo <444347292@qq.com>
2017-04-29 13:59:06 +0800

    wechat pay 封装一下

--- src/tools/wx_pay.js
+++ src/tools/wx_pay.js
@@ -18,6 +18,9 @@ const APP_ID = 'wx90eb6b04dcbf5fb2';
 const APP_SECRET = '7e87a72a56080c466d9256387016c886';
 const MCH_ID = '1462750902';
 const TRADE_TYPE = 'JSAPI';
+const NOTIFY_URL = 'http://i-sharing.xyz/api/wechat/order/notify';
+
+
 const MCH_KEY = 'guohaoweilovechengxihuiforeveruu';//key为在微信商户平台(pay.weixin.qq.com)-->账户设置-->API安全-->密钥设置
 
 const MCH_PAU_URL = 'https://api.mch.weixin.qq.com/';
@@ -28,11 +31,11 @@ const URL_REFUND = `${ MCH_PAU_URL }secapi/pay/refund`;
 const URL_WX_OPEN_ID_ACCESS_TOKEN = `https://api.weixin.qq.com/sns/oauth2/access_token?appid=${ APP_ID }&secret=${ APP_SECRET }&code=CODE&grant_type=authorization_code`;
 
 const _paySign = data => {
-  let string = `${ _raw(data) }&key=${MCH_KEY}`;
+  let string = `${ _sortAndGenerateParam(data) }&key=${MCH_KEY}`;
   return md5(string).toUpperCase();
 };
 
-const _raw = args => {
+const _sortAndGenerateParam = args => {
   let keys = Object.keys(args);
   keys = keys.sort();
   let newArgs = {};
@@ -46,8 +49,7 @@ const _raw = args => {
   return string;
 };
 
-const _payApiRequest = async (data, url) => {
-  logger.log('_payApiRequest');
+const _addSignAndConvertToXml = data => {
   let sign = _paySign(data);
 
   let formData = {
@@ -55,25 +57,18 @@ const _payApiRequest = async (data, url) => {
   };
 
   formData = json2xml(formData);
-  logger.log('test --');
-  let response = await superagent
-    .post(url)
-    .send(formData)
-    .charset(config.common.char_set_utf8);
-  logger.log('test --');
-  // logger.log(response.text);
-  let result = xml2json.toJson(response.text);
-  logger.log(result);
-
-  return JSON.parse(result);
+  return formData;
 };
 
+const _xml2JsonObj = xmlStr => {
+  logger.log(xmlStr);
+  return JSON.parse(xml2json.toJson(xmlStr));
+};
 
 const unifiedOrder = async ctx => {
   let body = ctx.query.body;
   let spbillCreateIp = ctx.ip;
 
-  let notifyUrl = 'http://i-sharing.xyz/api/wechat/order/notify';
   let totalFee = Number.parseInt(ctx.query.fee);
   let nonceStr = Math.random().toString();
   let openid = ctx.cookies.get(config.session.wxOpenId);
@@ -84,7 +79,7 @@ const unifiedOrder = async ctx => {
     body: body,// 商品或支付单简要描述
     mch_id: MCH_ID,// 商户号
     nonce_str: nonceStr,// 随机字符串，不长于32位
-    notify_url: notifyUrl,// 支付成功后微信服务器通过POST请求通知这个地址
+    notify_url: NOTIFY_URL,// 支付成功后微信服务器通过POST请求通知这个地址
     openid: openid,// 为微信用户在商户对应appid下的唯一标识
     out_trade_no: outTradeNo,//订单号
     spbill_create_ip: spbillCreateIp,//终端IP
@@ -92,7 +87,13 @@ const unifiedOrder = async ctx => {
     trade_type: TRADE_TYPE,//NATIVE会返回code_url ，JSAPI不会返回
   };
 
-  return await _payApiRequest(data, URL_UNIFIED_ORDER);
+  let formData = _addSignAndConvertToXml(data);
+  let response = await superagent
+    .post(URL_UNIFIED_ORDER)
+    .send(formData)
+    .charset(config.common.char_set_utf8);
+
+  return _xml2JsonObj(response.text);
 };
 
 const refund = async param => {
@@ -112,7 +113,13 @@ const refund = async param => {
     transaction_id: ''
   };
 
-  return await _payApiRequest(data, URL_REFUND);
+  let formData = _addSignAndConvertToXml(data);
+  let response = await superagent
+    .post(URL_REFUND)
+    .send(formData)
+    .charset(config.common.char_set_utf8);
+
+  return _xml2JsonObj(response.text);
 };
 
 const getAccessTokenOpenId = async code => {
