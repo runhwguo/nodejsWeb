8e60b4dd314154ae8b6e17dfbde801c12011acb6
runhwguo <444347292@qq.com>
2017-04-29 12:48:58 +0800

    wechat pay use third party is so bad, i create it

--- commitAndPush.sh
+++ commitAndPush.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 git add *
-git commit -m "wechat pay use third party, test"
+git commit -m "wechat pay use third party is so bad, i create it"
 git push
--- src/controllers/rest/wechat.js
+++ src/controllers/rest/wechat.js
@@ -48,9 +48,12 @@ const startPay = async ctx => {
   let request = '';
 
   if (openId) {
-    request = await wxPay.getOnBridgeReadyRequest(ctx);
+    let result = await wxPay.unifiedOrder(ctx);
+    let prepay_id = result.xml.prepay_id;
+    // logger.log('prepay_id = ' + prepay_id);
+    request = await wxPay.getOnBridgeReadyRequest(prepay_id);
   }
-  logger.log(request);
+  // logger.log(request);
   ctx.rest(request);
 }
 
