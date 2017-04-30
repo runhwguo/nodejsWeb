2aae402cfb55ae9900e877ad38bb603690eaee36
runhwguo <444347292@qq.com>
2017-04-30 12:20:59 +0800

    wechat pay notify url

--- src/controllers/rest/task.js
+++ src/controllers/rest/task.js
@@ -70,7 +70,7 @@ const get = async ctx => {
       order: [
         ['state', 'ASC'],
         ['priority', 'DESC'],
-        ['createdAt', 'ASC']
+        ['createdAt', 'DESC']
       ]
     });
   }
