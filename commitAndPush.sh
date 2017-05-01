c26e1318f696c66c406fd86df3955679a7a028ef
runhwguo <444347292@qq.com>
2017-05-01 17:42:53 +0800

    npm目前有问题，改写发布脚本

--- commitAndPush.sh
+++ commitAndPush.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 git add *
-git commit -m "判断task的状态属于谁，谁接单"
+git commit -m "npm目前有问题，改写发布脚本"
 git push
 git status
--- pullAndPublish.sh
+++ pullAndPublish.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-git pull
-npm test
+git pull && cd build && gulp && cd ..
+NODE_ENV=test
+node --use_strict start.js
