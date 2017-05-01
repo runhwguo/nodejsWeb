3f1ac3480489183bb8aaf90041703f6c83a6fea7
runhwguo <444347292@qq.com>
2017-05-01 17:58:02 +0800

    会员共享 生成单

--- pullAndPublish.sh
+++ pullAndPublish.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env bash
-git pull && cd build && gulp && cd ..
-NODE_ENV=test
-node --use_strict start.js
+git pull && cd build && gulp && cd .. && NODE_ENV=test && node --use_strict start.js
