bc1ac1bb24403a12e27b8981c88185910d80bf88
runhwguo <444347292@qq.com>
2017-05-01 10:31:30 +0800

    delete some log print

--- src/tools/controller.js
+++ src/tools/controller.js
@@ -30,23 +30,23 @@ const addMapping = (router, mapping) => {
     if (url.startsWith('GET ')) {
       let path = url.substring(4);
       router.get(path, mapping[url]);
-      console.log(`register URL mapping: GET ${path}`);
+      // console.log(`register URL mapping: GET ${path}`);
     } else if (url.startsWith('POST ')) {
       let path = url.substring(5);
       router.post(path, mapping[url]);
-      console.log(`register URL mapping: POST ${path}`);
+      // console.log(`register URL mapping: POST ${path}`);
     } else if (url.startsWith('PUT ')) {
       let path = url.substring(4);
       router.put(path, mapping[url]);
-      console.log(`register URL mapping: PUT ${path}`);
+      // console.log(`register URL mapping: PUT ${path}`);
     } else if (url.startsWith('DELETE ')) {
       let path = url.substring(7);
       router.del(path, mapping[url]);
-      console.log(`register URL mapping: DELETE ${path}`);
+      // console.log(`register URL mapping: DELETE ${path}`);
     } else if (url.startsWith('ALL ')) {
       let path = url.substring(4);
       router.all(path, mapping[url]);
-      console.log(`register URL mapping: ALL ${path}`);
+      // console.log(`register URL mapping: ALL ${path}`);
     } else {
       console.log('invalid URL: ${url}');
     }
