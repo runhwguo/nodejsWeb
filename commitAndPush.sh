075d78bf9d4ec568100f8ffae3ad94462399b17a
RunhwGuo <444347292@qq.com>
2017-05-02 10:45:59 +0800

    服务器重装node

--- commitAndPush.sh
+++ commitAndPush.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 git add *
-git commit -m "编写管理员 登录页面  服务器重装node"
+git commit -m "服务器重装node"
 git push
 git status
--- pullAndPublish.sh
+++ pullAndPublish.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-git pull && cd build && gulp && cd .. && NODE_ENV=test && node --use_strict start.js
+git pull
+npm test
