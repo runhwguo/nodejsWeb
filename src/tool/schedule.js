acb9d4223448ed7d4fcd309bed7c58dd784a0d1a
RunhwGuo <444347292@qq.com>
2017-05-04 20:44:03 +0800

    koa body xml set explicitArray false

--- src/tool/schedule.js
+++ src/tool/schedule.js
@@ -17,7 +17,7 @@ const setSchedule = () => {
   let scanRule = new schedule.RecurrenceRule();
 
   scanRule.hour = 20;
-  scanRule.minute = 43;
+  scanRule.minute = 45;
 
   let job = schedule.scheduleJob(scanRule, async () => {
     console.log('run schedule start...');
