1222e8ac43a1b5e890f7bc295228a92bc300d76c
runhwguo <444347292@qq.com>
2017-05-09 16:09:00 +0800

    增加 image压缩gulp

--- build/gulpfile.babel.js
+++ build/gulpfile.babel.js
@@ -2,19 +2,20 @@ import gulp from "gulp";
 import uglifyJS from "gulp-uglify";
 import cleanCss from "gulp-clean-css";
 import minHtml from "gulp-htmlmin";
+import minImage from "gulp-imagemin";
 import gutil from "gulp-util";
 import gulpBabel from "gulp-babel";
-
+import cache from "gulp-cache";
 
 gulp.task('min-html', () => {
   gulp.src('../view/**/*.html')
-    .pipe(minHtml({
+    .pipe(cache(minHtml({
       removeComments: true,//清除HTML注释
       collapseWhitespace: true,//压缩HTML
       minifyJS: true,//压缩页面JS
       minifyCSS: true//压缩页面CSS
-    }))
-    .on('error', (err) => {
+    })))
+    .on('error', err => {
       gutil.log(gutil.colors.red('[Error]'), err.toString());
     })
     .pipe(gulp.dest('../view-min'));
@@ -22,23 +23,42 @@ gulp.task('min-html', () => {
 
 gulp.task('clean-css', () => {
   gulp.src('../static/css/**/*.css')
-    .pipe(cleanCss())
-    .on('error', (err) => {
+    .pipe(cache(cleanCss()))
+    .pipe(gulp.dest('../static/css-min'))
+    .on('error', err => {
       gutil.log(gutil.colors.red('[Error]'), err.toString());
-    })
-    .pipe(gulp.dest('../static/css-min'));
+    });
 });
 
 gulp.task('babel-minify-js', () => {
   gulp.src('../static/js/**/*.js')
-    .pipe(gulpBabel({
+    .pipe(cache(gulpBabel({
       presets: ['es2015']
-    }))
-    .pipe(uglifyJS())
-    .on('error', function (err) {
+    })))
+    .pipe(cache(uglifyJS()))
+    .on('error', err => {
       gutil.log(gutil.colors.red('[Error]'), err.toString());
     })
     .pipe(gulp.dest('../static/js-min'));
 });
 
-gulp.task('default', ['min-html', 'clean-css', 'babel-minify-js']);
+gulp.task('min-image', () => {
+  gulp.src('../static/image/*')
+    .pipe(cache(minImage([
+      minImage.gifsicle({interlaced: true}),
+      minImage.jpegtran({progressive: true}),
+      minImage.optipng({optimizationLevel: 5}),
+      minImage.svgo({plugins: [{removeViewBox: true}]})
+    ]), {
+      verbose: true
+    }))
+    .on('error', err => {
+      gutil.log(gutil.colors.red('[Error]'), err.toString());
+    })
+    .pipe(gulp.dest('../static/image-test'))
+    .on('error', function (err) {
+      gutil.log(gutil.colors.red('[Error]'), err.toString());
+    });
+});
+
+gulp.task('default', ['min-html', 'clean-css', 'babel-minify-js', 'min-image']);
--- commitAndPush.sh
+++ commitAndPush.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 git add *
-git commit -m "给cookie设置时间"
+git commit -m "增加 image压缩gulp"
 git push
 git status
