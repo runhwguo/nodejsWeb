0517ca3054360684b9d3688aa62d6e923d11edd0
runhwguo <444347292@qq.com>
2017-05-11 01:00:03 +0800

    前端模块化 去dom操作 important！！   给接单增加预付逻辑

--- static/js/wx_pay.js
+++ static/js/wx_pay.js
@@ -9,11 +9,11 @@ function _isSupportWxPay() {
 
 function onBridgeReady() {
   $.get(`/api/wechat/pay/start?` + $.param(_param), data => {
-      alert(data);
+      alert(JSON.stringify(data));
       if (data) {
         WeixinJSBridge.invoke('getBrandWCPayRequest', data, res => {
             // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
-            alert(res);
+            alert(JSON.stringify(res));
             if ('get_brand_wcpay_request:ok' === res.err_msg) {
               if (_successCallback) {
                 _successCallback();
