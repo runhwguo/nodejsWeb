7469e28d4436b808b02673721b9b71b016823289
runhwguo <444347292@qq.com>
2017-05-16 22:24:21 +0800

    min-image在linux上失败  TypeError: _gulpImagemin2.default.gifsicle is not a function，目前也用不上，先注释

--- commitAndPush.sh
+++ commitAndPush.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 git add *
-git commit -m "修改.gitignore文件"
+git commit -m "min-image在linux上失败  TypeError: _gulpImagemin2.default.gifsicle is not a function，目前也用不上，先注释"
 git push
 git status
