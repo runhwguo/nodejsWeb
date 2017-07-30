1fe9e768a8e69c830a5ef25eec827464122c2023
runhwguo <444347292@qq.com>
2017-07-30 13:20:07 +0800

    优化 .gitignore

--- script/commitAndPush.sh
+++ script/commitAndPush.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo 'should execute in project root direction,like ./script/**.sh'
 git add *
-git commit -m "1.进入首页，不需要出发login的逻辑，优化！2.test阶段会memory out 有待观察"
+git commit -m "优化 .gitignore"
 git push
 git status
