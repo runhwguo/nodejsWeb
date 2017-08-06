4fbf4da43bbf7a801b9b20aa43910b5c18e40c40
runhwguo <444347292@qq.com>
2017-08-06 16:22:33 +0800

    1.安装cnpm 2.增加startProcess.sh脚本 3.脚本应该在script目录下执行

--- script/commitAndPush.sh
+++ script/commitAndPush.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-echo 'should execute in project root direction,like ./script/**.sh'
+echo 'should execute in project script direction'
+cd ..
 git add *
-git commit -m "1.安装cnpm 2.增加startProcess.sh脚本"
+git commit -m "1.安装cnpm 2.增加startProcess.sh脚本 3.脚本应该在script目录下执行"
 git push
 git status
+cd script
