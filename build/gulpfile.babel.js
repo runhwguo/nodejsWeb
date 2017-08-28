2e205b25662ee4b74db389a7f28a2a5a86acc7f7
runhwguo <444347292@qq.com>
2017-08-28 23:08:53 +0800

    1.production environment dist file copy

--- build/gulpfile.babel.js
+++ build/gulpfile.babel.js
@@ -7,9 +7,15 @@ import gutil from 'gulp-util';
 import gulpBabel from 'gulp-babel';
 import cache from 'gulp-cache';
 import eslint from 'gulp-eslint';
+import Path from 'path';
+
+const DEST_DIR   = Path.join('..', 'dist'),
+      STATIC_DIR = Path.join('..', 'static'),
+      VIEW_DIR   = 'view';
 
 gulp.task('min-html', () => {
-  gulp.src('../view/**/*.html')
+  // gulp.src('../view/**/*.html')
+  gulp.src(Path.join('..', VIEW_DIR, '**', '*.html'))
     .pipe(cache(minHtml({
       removeComments: true,//清除HTML注释
       collapseWhitespace: true,//压缩HTML
@@ -19,13 +25,14 @@ gulp.task('min-html', () => {
     .on('error', err => {
       gutil.log(gutil.colors.red('[Error]'), err.toString());
     })
-    .pipe(gulp.dest('../dist/view'));
+    .pipe(gulp.dest(Path.join(DEST_DIR, VIEW_DIR)));
 });
 
 gulp.task('clean-css', () => {
-  gulp.src('../static/css/**/*.css')
+  // gulp.src('../static/css/**/*.css')
+  gulp.src(Path.join(STATIC_DIR, 'css', '**', '*.css'))
     .pipe(cache(cleanCss()))
-    .pipe(gulp.dest('../dist/css'))
+    .pipe(gulp.dest(Path.join(DEST_DIR, 'css')))
     .on('error', err => {
       gutil.log(gutil.colors.red('[Error]'), err.toString());
     });
@@ -91,7 +98,8 @@ gulp.task('babel-minify-js', () => {
 });
 
 gulp.task('min-image', () => {
-  gulp.src('../static/image/*')
+  // gulp.src('../static/image/*')
+  gulp.src(Path.join(STATIC_DIR, 'image', '*'))
     .pipe(cache(minImage([
       minImage.gifsicle({interlaced: true}),
       minImage.jpegtran({progressive: true}),
@@ -103,10 +111,24 @@ gulp.task('min-image', () => {
     .on('error', err => {
       gutil.log(gutil.colors.red('[Error]'), err.toString());
     })
-    .pipe(gulp.dest('../dist/image'))
-    .on('error', function (err) {
+    .pipe(gulp.dest(Path.join(DEST_DIR, 'image')))
+    .on('error', err => {
       gutil.log(gutil.colors.red('[Error]'), err.toString());
     });
 });
 
-gulp.task('default', ['min-html', 'clean-css', 'lint', 'babel-minify-js', 'min-image']);
+gulp.task('copy', () => {
+  gulp.src(Path.join(STATIC_DIR, 'font', '*'))
+    .pipe(gulp.dest(Path.join(DEST_DIR, 'font')))
+    .on('error', err => {
+      gutil.log(gutil.colors.red('[Error]'), err.toString());
+    });
+
+  gulp.src(Path.join(STATIC_DIR, 'libs', '**', '*'))
+    .pipe(gulp.dest(Path.join(DEST_DIR, 'libs')))
+    .on('error', err => {
+      gutil.log(gutil.colors.red('[Error]'), err.toString());
+    });
+});
+
+gulp.task('default', ['min-html', 'clean-css', 'lint', 'babel-minify-js', 'min-image', 'copy']);
