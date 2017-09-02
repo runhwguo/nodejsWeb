c4f7c8aa61059d9c86e975702b8a567996e641ac
runhwguo <444347292@qq.com>
2017-09-02 19:43:43 +0800

    1. wechat pay need to check client ip, but we use nginx to proxy, so need to set real ip in node koa and nginx.conf

--- static/js/wx_pay.js
+++ static/js/wx_pay.js
@@ -13,10 +13,10 @@ function onBridgeReady() {
           data,
           res => {
             // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
-            alert(JSON.stringify(res));
             if ('get_brand_wcpay_request:ok' === res.err_msg) {
               _successCallback && _successCallback();
             } else {
+              alert(JSON.stringify(res));
               _failCallback && _failCallback();
             }
           }
