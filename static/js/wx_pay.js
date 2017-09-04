6407cb1b7004ca3ead19f50a9330ddc5e1cc89ea
runhwguo <444347292@qq.com>
2017-09-04 18:26:41 +0800

    1. wechat pay add some logs

--- static/js/wx_pay.js
+++ static/js/wx_pay.js
@@ -9,8 +9,7 @@ function onBridgeReady() {
   $.get(`/api/wechat/pay/start?${$.param(_param)}`,
     data => {
       if (data) {
-        WeixinJSBridge.invoke('getBrandWCPayRequest',
-          data,
+        WeixinJSBridge.invoke('getBrandWCPayRequest', data,
           res => {
             // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
             if ('get_brand_wcpay_request:ok' === res.err_msg) {
